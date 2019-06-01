6db0b6ce7f4c52e0311522fdd4bb059a29ca42a1
Patrycja Bednarska <s16976@pjwstk.edu.pl>
2019-06-01 22:27:37 +0200

    first commit

---
+++ src/main/java/domain/Comment.java
@@ -0,0 +1,45 @@
+package domain;
+
+import javax.persistence.*;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement
+@Entity
+@NamedQueries({
+        @NamedQuery(name = "comment.all", query="SELECT p FROM Comment p"),
+        @NamedQuery(name = "comment.id", query="FROM Comment p WHERE p.id=:commentId"),
+})
+public class Comment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String text;
+
+    @ManyToOne
+    private Product product;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+}
